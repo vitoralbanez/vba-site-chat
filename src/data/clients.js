81e478b17ac9245818662b561dfad4c254e0648b
Vitor B. Albanez <albanez.vitor@gmail.com>
2026-03-13 01:17:34 -0300

    Add client images to clients.js

--- src/data/clients.js
+++ src/data/clients.js
@@ -1,1 +1,19 @@
+import cliente1 from "../assets/logos/ATC.png";
+import cliente2 from "../assets/logos/JBFenix.png";
+import cliente3 from "../assets/logos/crosshard.png";
+import cliente4 from "../assets/logos/haddad.png";
+import cliente5 from "../assets/logos/nutritruck.png";
+import cliente6 from "../assets/logos/eservice.png";
+import cliente7 from "../assets/logos/evidence.png";
 
+const clients = [
+  { name: "Cliente 1", image: cliente1 },
+  { name: "Cliente 2", image: cliente2 },
+  { name: "Cliente 3", image: cliente3 },
+  { name: "Cliente 4", image: cliente4 },
+  { name: "Cliente 5", image: cliente5 },
+  { name: "Cliente 6", image: cliente6 },
+  { name: "Cliente 7", image: cliente7 },
+];
+
+export default clients;
