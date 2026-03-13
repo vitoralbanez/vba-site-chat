948f8d031cd803febb54e089a1ccc5b2e2e33aa8
Vitor B. Albanez <albanez.vitor@gmail.com>
2026-03-13 16:52:15 -0300

    Remove duplicate CrossHard import and client entry
    Removed duplicate import and entry for CrossHard.

--- src/data/clients.js
+++ src/data/clients.js
@@ -1,15 +1,15 @@
+import CrossHard from "../assets/logos/crosshard.png";
 import ATC from "../assets/logos/ATC.png";
 import JBFenix from "../assets/logos/JBFenix.png";
-import CrossHard from "../assets/logos/crosshard.png";
 import Haddad from "../assets/logos/haddad.png";
 import NutriTruck from "../assets/logos/nutritruck.png";
 import eService from "../assets/logos/eservice.png";
 import Evidence from "../assets/logos/evidence.png";
 
 const clients = [
+  { name: "CrossHard", image: CrossHard },
   { name: "ATC", image: ATC },
   { name: "JBFenix", image: JBFenix },
-  { name: "CrossHard", image: CrossHard },
   { name: "Haddad", image: Haddad },
   { name: "NutriTruck", image: NutriTruck },
   { name: "eService", image: eService },
