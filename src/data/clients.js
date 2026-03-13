7f4488854ceceb409e8baa127d713e3d2f1f4a8c
Vitor B. Albanez <albanez.vitor@gmail.com>
2026-03-13 01:26:45 -0300

    Refactor client imports and update client names

--- src/data/clients.js
+++ src/data/clients.js
@@ -1,19 +1,19 @@
-import cliente1 from "../assets/logos/ATC.png";
-import cliente2 from "../assets/logos/JBFenix.png";
-import cliente3 from "../assets/logos/crosshard.png";
-import cliente4 from "../assets/logos/haddad.png";
-import cliente5 from "../assets/logos/nutritruck.png";
-import cliente6 from "../assets/logos/eservice.png";
-import cliente7 from "../assets/logos/evidence.png";
+import ATC from "../assets/logos/ATC.png";
+import JBFenix from "../assets/logos/JBFenix.png";
+import CrossHard from "../assets/logos/crosshard.png";
+import Haddad from "../assets/logos/haddad.png";
+import NutriTruck from "../assets/logos/nutritruck.png";
+import eService from "../assets/logos/eservice.png";
+import Evidence from "../assets/logos/evidence.png";
 
 const clients = [
-  { name: "Cliente 1", image: cliente1 },
-  { name: "Cliente 2", image: cliente2 },
-  { name: "Cliente 3", image: cliente3 },
-  { name: "Cliente 4", image: cliente4 },
-  { name: "Cliente 5", image: cliente5 },
-  { name: "Cliente 6", image: cliente6 },
-  { name: "Cliente 7", image: cliente7 },
+  { name: "ATC", image: ATC },
+  { name: "JBFenix", image: JBFenix },
+  { name: "CrossHard", image: CrossHard },
+  { name: "Haddad", image: Haddad },
+  { name: "NutriTruck", image: NutriTruck },
+  { name: "eService", image: eService },
+  { name: "Evidence", image: Evidence },
 ];
 
 export default clients;
