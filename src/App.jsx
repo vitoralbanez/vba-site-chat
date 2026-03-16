c7fa9347172d9718b64b9292c9250fa7b56bd29b
Vitor B. Albanez <albanez.vitor@gmail.com>
2026-03-16 12:35:07 -0300

    CorrecaoMain+APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
-import ChatSolutions from "./pages/ChatSolutions"
-
-
+import ChatSolutions from "./pages/ChatSolutions";
 
 export default function App() {
   return (
-        <Routes>
-
-      <Route path="/" element={<Home />} />
-
-      <Route path="/chat" element={<ChatSolutions />} />
-
-    </Routes>
-  )
-  return (
     <>
       <Navbar />
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<ChatSolutions />} />
+      </Routes>
+
       <Footer />
     </>
-    
   );
 }
